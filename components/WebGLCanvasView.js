7281b472c24d89f90cb72ecf1eaf478ef961f982
kosooyoul <kosooyoul@gmail.com>
2022-07-23 16:17:44 +0900

    🌱 Upt. add joypad to practice of fall 3d

--- components/WebGLCanvasView.js
+++ components/WebGLCanvasView.js
@@ -1,10 +1,25 @@
 class WebGLCanvasView {
-
-    _y = 2000;
+    _y = 4100;
     _speedY = 0;
     _maxSpeedY = 8;
     _gravity = 0.005;
     _groundReflectivity = 0.6;
+
+    _width = 0;
+    _height = 0;
+    _scale = 1;
+    _scaledHalfWidth = 0;
+    _scaledHalfHeight = 0;
+    _boundary = {
+        left: 0,
+        top: 0,
+        right: 0,
+        bottom: 0
+    };
+
+    _tick = 0;
+
+    _joypad;
 
     constructor(canvas) {
         console.log("initialize");
@@ -26,7 +41,7 @@ class WebGLCanvasView {
 
         this._scene = new THREE.Scene();
         this._camera = new THREE.PerspectiveCamera(60, this._canvas.clientWidth / this._canvas.clientHeight, 0.1, 1000);
-        this._camera.rotateX(Math.PI * -0.5);
+        // this._camera.rotateX(Math.PI * -0.5);
         this._camera.position.y = this._y;
 
         const sphere = new THREE.Group();
@@ -49,10 +64,10 @@ class WebGLCanvasView {
         directionalLight3.position.z = 100;
         this._scene.add(directionalLight3);
 
-        for (let i = 0; i < 2000; i++) {
+        for (let i = 0; i < 1000; i++) {
             let left = Math.random() * 200 - 100;
             let right = Math.random() * 200 - 100;
-            let top = Math.random() * 2000 + 10;
+            let top = Math.random() * 4000 + 10;
             let length = Math.random() * 10 + 1;
             this.createBlock(sphere, left, right, top, length);
         }
@@ -62,6 +77,21 @@ class WebGLCanvasView {
 
         // Autoplay
         this.play();
+
+        canvas.addEventListener("mousedown", (evt) => this._onPointersDown(evt));
+        canvas.addEventListener("mousemove", (evt) => this._onPointersMove(evt));
+        canvas.addEventListener("mouseup", (evt) => this._onPointersUp(evt));
+        canvas.addEventListener("mouseout", (evt) => this._onPointersUp(evt));
+        canvas.addEventListener("mouseleave", (evt) => this._onPointersUp(evt));
+        canvas.addEventListener("touchstart", (evt) => this._onPointersDown(evt));
+        canvas.addEventListener("touchmove", (evt) => this._onPointersMove(evt));
+        canvas.addEventListener("touchend", (evt) => this._onPointersUp(evt));
+        document.body.addEventListener("keydown", (evt) => this._onKeyDown(evt));
+        document.body.addEventListener("keyup", (evt) => this._onKeyUp(evt));
+    }
+
+    setJoypad(joypad) {
+        this._joypad = joypad;
     }
 
     getSphere() {
@@ -101,9 +131,34 @@ class WebGLCanvasView {
     }
 
     _compute() {
+        this._tick++;
+
         this._renderer.setSize(this._canvas.clientWidth, this._canvas.clientHeight, false);
         this._camera.aspect = this._canvas.clientWidth / this._canvas.clientHeight;
+        this._camera.updateProjectionMatrix();
 
+        this._width = this._canvas.width;
+        this._height = this._canvas.height;
+        this._scaledHalfWidth = this._width / 2 / this._scale;
+        this._scaledHalfHeight = this._height / 2 / this._scale;
+        this._boundary = {
+            left: -this._width / 2 / this._scale,
+            right: this._width / 2 / this._scale,
+            top: -this._height / 2 / this._scale,
+            bottom: this._height / 2 / this._scale
+        };
+
+        this._joypad && this._joypad.setCanvasSize(this._canvas.width, this._canvas.height, this._scale);
+
+        const status = {
+            joypad: this._joypad && this._joypad.getStatus(),
+            boundary: this._boundary,
+            tick: this._tick
+        };
+
+        this._joypad && this._joypad.compute(status);
+
+        // Custom
         this._speedY += this._gravity;
         if (this._speedY > this._maxSpeedY) {
             this._speedY = this._maxSpeedY;
@@ -115,8 +170,12 @@ class WebGLCanvasView {
         }
 
         this._camera.position.y = this._y;
-        this._camera.rotateZ(Math.PI * -0.001);
-        this._camera.updateProjectionMatrix();
+        if (status.joypad["left"]) {
+            this._camera.rotation.z += (Math.PI * -0.01);
+        } else if (status.joypad["right"]) {
+            this._camera.rotation.z += (Math.PI * 0.01);
+        }
+        this._camera.rotation.x = Math.PI * -0.5;
     }
 
     _render(_context) {
@@ -144,5 +203,58 @@ class WebGLCanvasView {
         this._playing = false;
 
         // Destroy All Objects
+    }
+
+    _getPointers(evt) {
+        var touches = evt.targetTouches ? evt.targetTouches : [evt];
+        const pointers = [];
+        for (var i = 0; i < touches.length; i++) {
+            pointers.push({
+                x: touches[i].pageX,
+                y: touches[i].pageY,
+                id: touches[i].identifier
+            });
+        }
+        return pointers;
+    }
+
+    _onPointersDown(evt) {
+        if (evt.type == "touchstart") {
+            evt.preventDefault(); // for Mobile
+        }
+
+        var pointers = this._getPointers(evt);
+
+        if (this._joypad) {
+            this._joypad.onPointersDown(pointers);
+        }
+    }
+
+    _onPointersMove(evt) {
+        var pointers = this._getPointers(evt);
+
+        if (this._joypad) {
+            this._joypad.onPointersMove(pointers);
+        }
+    }
+
+    _onPointersUp(evt) {
+        var pointers = this._getPointers(evt);
+
+        if (this._joypad) {
+            this._joypad.onPointersUp(pointers);
+        }
+    }
+
+    _onKeyDown(evt) {
+        if (this._joypad) {
+            this._joypad.onKeyDown(evt.which);
+        }
+    }
+
+    _onKeyUp(evt) {
+        if (this._joypad) {
+            this._joypad.onKeyUp(evt.which);
+        }
     }
 }
