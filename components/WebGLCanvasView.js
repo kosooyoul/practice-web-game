5f0ff4dc70197a56a8323c26465b7c51e57f3930
kosooyoul <kosooyoul@gmail.com>
2022-07-23 15:56:24 +0900

    🌱 Upt. add falling and random blocks, lights

--- components/WebGLCanvasView.js
+++ components/WebGLCanvasView.js
@@ -1,4 +1,11 @@
 class WebGLCanvasView {
+
+    _y = 2000;
+    _speedY = 0;
+    _maxSpeedY = 8;
+    _gravity = 0.005;
+    _groundReflectivity = 0.6;
+
     constructor(canvas) {
         console.log("initialize");
 
@@ -15,14 +22,42 @@ class WebGLCanvasView {
         this._context = this._renderer.getContext();
 
         this._renderer.setSize(this._canvas.clientWidth, this._canvas.clientHeight, false);
-        this._renderer.setClearColor(0xF9F9F9, 1);
+        this._renderer.setClearColor(0x000000, 1);
 
         this._scene = new THREE.Scene();
         this._camera = new THREE.PerspectiveCamera(60, this._canvas.clientWidth / this._canvas.clientHeight, 0.1, 1000);
+        this._camera.rotateX(Math.PI * -0.5);
+        this._camera.position.y = this._y;
 
         const sphere = new THREE.Group();
-        sphere.add(this.getSphere());
 
+        const directionalLight1 = new THREE.DirectionalLight(0xffff00, 0.4);
+        directionalLight1.position.x = 100;
+        directionalLight1.position.y = 100;
+        directionalLight1.position.z = 1;
+        this._scene.add(directionalLight1);
+
+        const directionalLight2 = new THREE.DirectionalLight(0x00ffff, 0.4);
+        directionalLight2.position.x = -100;
+        directionalLight2.position.y = 100;
+        directionalLight2.position.z = 1;
+        this._scene.add(directionalLight2);
+
+        const directionalLight3 = new THREE.DirectionalLight(0xff00ff, 0.4);
+        directionalLight3.position.x = 100;
+        directionalLight3.position.y = 100;
+        directionalLight3.position.z = 100;
+        this._scene.add(directionalLight3);
+
+        for (let i = 0; i < 2000; i++) {
+            let left = Math.random() * 200 - 100;
+            let right = Math.random() * 200 - 100;
+            let top = Math.random() * 2000 + 10;
+            let length = Math.random() * 10 + 1;
+            this.createBlock(sphere, left, right, top, length);
+        }
+
+        this.createGround(sphere, 0)
         this._scene.add(sphere);
 
         // Autoplay
@@ -31,8 +66,26 @@ class WebGLCanvasView {
 
     getSphere() {
         var geometry = new THREE.SphereGeometry(100, 40, 40, 0, Math.PI * 2, 0, Math.PI * 2);
-        var material = new THREE.MeshBasicMaterial({ color: 0xffa0a0, wireframe: true, side: THREE.DoubleSide });
+        var material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true, side: THREE.DoubleSide });
         return new THREE.Mesh(geometry, material);
+    }
+
+    createBlock(parent, x, y, z, length) {
+        const geometry = new THREE.BoxGeometry(0.5, length, 0.5);
+        const material = new THREE.MeshPhongMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.translateX(x);
+        mesh.translateZ(y);
+        mesh.translateY(z);
+        parent.add(mesh);
+    }
+
+    createGround(parent, z) {
+        const geometry = new THREE.BoxGeometry(100, 1, 100);
+        const material = new THREE.MeshPhongMaterial({ color: 0x999999 });
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.translateY(z);
+        parent.add(mesh);
     }
 
     _requestLoop(context) {
@@ -50,6 +103,19 @@ class WebGLCanvasView {
     _compute() {
         this._renderer.setSize(this._canvas.clientWidth, this._canvas.clientHeight, false);
         this._camera.aspect = this._canvas.clientWidth / this._canvas.clientHeight;
+
+        this._speedY += this._gravity;
+        if (this._speedY > this._maxSpeedY) {
+            this._speedY = this._maxSpeedY;
+        }
+        this._y -= this._speedY;
+        if (this._y < 1) {
+            this._y = 1;
+            this._speedY = -this._speedY * this._groundReflectivity;
+        }
+
+        this._camera.position.y = this._y;
+        this._camera.rotateZ(Math.PI * -0.001);
         this._camera.updateProjectionMatrix();
     }
 
